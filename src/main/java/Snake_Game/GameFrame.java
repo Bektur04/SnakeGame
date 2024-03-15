509a8bb2b0b7b08ee2c3734957e2872b20c85a4b
bekturisaev <bektur.isaev2004@gmail.com>
2024-03-15 02:55:51 -0500

    Changed

--- src/main/java/Snake_Game/GameFrame.java
+++ src/main/java/Snake_Game/GameFrame.java
@@ -1,3 +1,5 @@
+package Snake_Game;
+
 import javax.swing.*;
 
 public class GameFrame extends JFrame {
--- src/main/java/Snake_Game/SnakeGame.java
+++ src/main/java/Snake_Game/SnakeGame.java
@@ -1,3 +1,7 @@
+package Snake_Game;
+
+import Snake_Game.GameFrame;
+
 public class SnakeGame {
     public static void main(String[] args) {
         new GameFrame();
